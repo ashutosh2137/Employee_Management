277474cca2af633d86771e4e275bc16438b8a0d8
ashut <ashut@ashutosh>
2024-05-31 23:42:18 +0530

    first commit

---
+++ EmploieeManagement/src/main/java/com/employee/EmploieeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploieeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploieeManagementApplication.class, args);
+	}
+
+}
